7357db7037ad6e32caa0374c16d9c587afdde441
Kroese <kroese@users.noreply.github.com>
2023-12-11 10:57:54 +0100

    Test

--- src/server/utils/create-ssr-html.tsx
+++ src/server/utils/create-ssr-html.tsx
@@ -2,15 +2,12 @@ import { getStaticDir } from "@utils/env";
 import { Helmet } from "inferno-helmet";
 import { renderToString } from "inferno-server";
 import serialize from "serialize-javascript";
-import sharp from "sharp";
 import { favIconPngUrl, favIconUrl } from "../../shared/config";
 import { IsoDataOptionalSite } from "../../shared/interfaces";
 import { buildThemeList } from "./build-themes-list";
 import { fetchIconPng } from "./fetch-icon-png";
 
 const customHtmlHeader = process.env["LEMMY_UI_CUSTOM_HTML_HEADER"] || "";
-
-let appleTouchIcon: string | undefined = undefined;
 
 export async function createSsrHtml(
   root: string,
@@ -28,25 +25,6 @@ export async function createSsrHtml(
     customHtmlHeaderScriptTag,
     `<script nonce="${cspNonce}"`,
   );
-
-  if (!appleTouchIcon) {
-    appleTouchIcon = site?.site_view.site.icon
-      ? `data:image/png;base64,${await sharp(
-          await fetchIconPng(site.site_view.site.icon),
-        )
-          .resize(180, 180)
-          .extend({
-            bottom: 20,
-            top: 20,
-            left: 20,
-            right: 20,
-            background: "#222222",
-          })
-          .png()
-          .toBuffer()
-          .then(buf => buf.toString("base64"))}`
-      : favIconPngUrl;
-  }
 
   const erudaStr =
     process.env["LEMMY_UI_DEBUG"] === "true"
@@ -82,18 +60,7 @@ export async function createSsrHtml(
     <meta name="Description" content="Lemmy">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link
-       id="favicon"
-       rel="shortcut icon"
-       type="image/x-icon"
-       href=${site?.site_view.site.icon ?? favIconUrl}
-     />
-  
-    <!-- Web app manifest -->
-    <link rel="manifest" href="/manifest.webmanifest" />
-    <link rel="apple-touch-icon" href=${appleTouchIcon} />
-    <link rel="apple-touch-startup-image" href=${appleTouchIcon} />
-  
+     
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="${getStaticDir()}/styles/styles.css" />
   
