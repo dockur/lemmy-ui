927f8b5c47bdff8d032f3f042271ebb958456e82
Kroese <kroese@users.noreply.github.com>
2024-10-02 14:18:57 +0200

    Update footer.tsx

--- src/shared/components/app/footer.tsx
+++ src/shared/components/app/footer.tsx
@@ -25,42 +25,15 @@ export class Footer extends Component<FooterProps, any> {
               </li>
             )}
             <li className="nav-item">
-              <span className="nav-link">BE: {this.props.site?.version}</span>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/modlog">
-                {I18NextService.i18n.t("modlog")}
-              </NavLink>
+              <span className="nav-link">v{this.props.site?.version}</span>
             </li>
             {this.props.site?.site_view.local_site.legal_information && (
               <li className="nav-item">
                 <NavLink className="nav-link" to="/legal">
-                  {I18NextService.i18n.t("legal_information")}
+                  Contact
                 </NavLink>
               </li>
             )}
-            {this.props.site?.site_view.local_site.federation_enabled && (
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/instances">
-                  {I18NextService.i18n.t("instances")}
-                </NavLink>
-              </li>
-            )}
-            <li className="nav-item">
-              <a className="nav-link" href={docsUrl}>
-                {I18NextService.i18n.t("docs")}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={repoUrl}>
-                {I18NextService.i18n.t("code")}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={joinLemmyUrl}>
-                {new URL(joinLemmyUrl).hostname}
-              </a>
-            </li>
           </ul>
         </div>
       </footer>
